0f9e9190564d03d880db2fbbaefba4f1e2a276c7
Amatlakhova <alina.matlakhova@gmail.com>
2020-06-13 21:16:37 +0300

    Add testGetClassNumber

--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -3,14 +3,23 @@ import org.junit.Test;
 
 public class MainClassTest {
 
+    MainClass main = new MainClass();
+
     @Test
     public void testGetLocalNumber()
     {
-       MainClass main = new MainClass();
-
        int expected = 14;
        int actual = main.getLocalNumber();
 
         Assert.assertTrue("LocalNumber != 14", actual == expected);
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        int actual = main.getClassNumber();
+        int compared = 45;
+
+        Assert.assertTrue("ClassNumber <= 45", actual > compared);
     }
 }
