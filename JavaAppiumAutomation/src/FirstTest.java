682f5d2f6554489c113f6d81356631a3541e65f8
Amatlakhova <alina.matlakhova@gmail.com>
2020-07-12 23:47:35 +0300

    AfterTest orientation verification

--- JavaAppiumAutomation/src/FirstTest.java
+++ JavaAppiumAutomation/src/FirstTest.java
@@ -39,6 +39,9 @@ public class FirstTest {
     @After
     public void tearDown()
     {
+        if (driver.getOrientation() != ScreenOrientation.PORTRAIT) {
+            driver.rotate(ScreenOrientation.PORTRAIT);
+        }
         driver.quit();
     }
 
